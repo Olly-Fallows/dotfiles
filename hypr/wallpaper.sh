45bcb53c9bf95678368777bfe8328dfdfaf7c42d
Olly-Fallows <ollyfallows@outlook.com>
2026-05-22 12:25:37 +0100

    ini

---
+++ hypr/wallpaper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+hyprctl hyprpaper wallpaper "HDMI-A-1,~/Pictures/Wallpaper/newgame.jpg"
+hyprctl hyprpaper wallpaper "DP-1,~/Pictures/Wallpaper/newgame.jpg"
+hyprctl hyprpaper wallpaper "DP-2,~/Pictures/Wallpaper/newgame.jpg"
